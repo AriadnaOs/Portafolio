cddd4affb8c58362b0fa7e52d48dc4466a1a0ee5
AriadnaOs <ariadnao95@hotmail.com>
2022-03-08 14:51:15 -0600

    Section About and Form

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { subscribeOn } from 'rxjs-compat/operator/subscribeOn';
+import { Project } from '../../../models/projects';
+import { ProjectService } from '../../../service/project.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
+  providers: [ProjectService]
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+  public project: Project;
+  public status: string;
 
-  ngOnInit(): void {
+  constructor(
+    private _projectService: ProjectService
+  ) { 
+    this.title = "Create project";
+    this.project = new Project('', '', '', '', 2022, '', '');
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form){
+    
+    this._projectService.saveProject(this.project).subscribe(
+      response => {
+        if(response.project){
+          this.status ='success';
+          form.reset(); // To empty the form after completed
+        }else{
+          this.status ='failed';
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
   }
 
 }
