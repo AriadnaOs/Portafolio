2f859e06ce8bb651fe8ffaa933ef360cb3c61af1
AriadnaOs <ariadnao95@hotmail.com>
2022-03-10 11:39:46 -0600

    Improving the project section

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { subscribeOn } from 'rxjs-compat/operator/subscribeOn';
 import { Project } from '../../../models/projects';
 import { ProjectService } from '../../../service/project.service';
+import { UploadService } from '../../../service/upload.service';
+import { Global } from '../../../service/global';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers: [ProjectService]
+  providers: [ProjectService, UploadService]
 })
 export class CreateComponent implements OnInit {
 
   public title: string;
   public project: Project;
+  public save_project;
   public status: string;
+  public filesToUpload: Array<File>;
 
   constructor(
-    private _projectService: ProjectService
+    private _projectService: ProjectService,
+    private _uploadService: UploadService
   ) { 
     this.title = "Create project";
     this.project = new Project('', '', '', '', 2022, '', '');
@@ -27,11 +31,21 @@ export class CreateComponent implements OnInit {
 
   onSubmit(form){
     
+    // Save data
     this._projectService.saveProject(this.project).subscribe(
       response => {
         if(response.project){
-          this.status ='success';
-          form.reset(); // To empty the form after completed
+         
+          // Upload image
+          this._uploadService.makeFileRequest(Global.url+"upload-image/"+response.project._id, [], this.filesToUpload, 'image')
+          .then((result:any) =>{
+
+            this.save_project = response.project;
+
+            this.status ='success';
+            form.reset(); // To empty the form after completed
+          });
+          
         }else{
           this.status ='failed';
         }
@@ -40,6 +54,10 @@ export class CreateComponent implements OnInit {
         console.log(<any>error);
       }
     );
+  }
+
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
   }
 
 }
---
+++ src/app/components/detail/detail.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../../../models/projects';
+import { ProjectService } from '../../../service/project.service';
+import { Global } from '../../../service/global';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.css'],
+  providers: [ProjectService]
+})
+export class DetailComponent implements OnInit {
+  public url: string;
+  public project: Project;
+  constructor(
+    private _projectService: ProjectService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ){
+    this.url = Global.url;
+   }
+
+  ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      let id = params.id;
+
+      this.getProject(id);
+    });
+  }
+  getProject(id){
+    this._projectService.getProject(id).subscribe(
+      response => {
+        this.project = response.project;
+      },
+      error => {
+        console.log(<any>error);
+      }
+    )
+  }
+}
